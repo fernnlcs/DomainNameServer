597ca620595a577d151caa1d6a001595cdc96055
Fernando Silva <fernnlcs@gmail.com>
2022-11-23 17:11:45 -0300

    Feat: Simulator

--- src/main/core/DNSSimulator.java
+++ src/main/core/DNSSimulator.java
@@ -10,21 +10,42 @@ import main.utils.URI;
 
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class DNSSimulator {
 
     private final HashTable<URI, IPv4Address> table;
     private final Logger logger;
+    private final Timer timer = new Timer();
 
     public DNSSimulator(Logger logger) {
         this.logger = logger;
         this.table = new HashTable<>();
     }
 
-    public static String present() {
+    private void preset() {
+        // Programamos 25 sites a cada 10 segundos
+        this.timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                addSites(25);
+                logger.report();
+            }
+        }, 0, 10000);
+
+        this.logger.report();
+    }
+
+    private void end() {
+        this.timer.cancel();
+    }
+
+    private static String present() {
 
         return """
                 \t\tSERVIDOR DNS
+                \t\t------------
                 NÚMERO | Cadastrar N sites aleatórios
                 C | Cadastrar/atualizar site no servidor
                 V | Visualizar tabela hash dos sites cadastrados
@@ -34,35 +55,38 @@ public class DNSSimulator {
                 """;
     }
 
-    public void addSites(int quantity) {
+    private void addSites(int quantity) {
         for (int i = 0; i < quantity; i++) {
             URI uri = URIFactory.next();
             IPv4Address ip = IPv4Factory.next();
             this.table.add(uri, ip);
+        }
 
+        this.logger.log(quantity + " sites cadastrados.");
+    }
+
+    private void addSite(Scanner scanner) {
+        System.out.println("| Adicionar site...");
+
+        try {
+            System.out.print("URI: ");
+            URI uri = new URI(scanner.nextLine());
+
+            System.out.print("IP: ");
+            IPv4Address ip = new IPv4Address(scanner.nextLine());
+            this.table.add(uri, ip);
             this.logger.log("Site cadastrado: " + uri + " (" + ip + ")");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
         }
     }
 
-    public void addSite(Scanner scanner) {
-        System.out.println("| Adicionar site...");
-
-        System.out.print("URI: ");
-        URI uri = new URI(scanner.nextLine());
-
-        System.out.print("IP: ");
-        IPv4Address ip = new IPv4Address(scanner.nextLine());
-
-        this.table.add(uri, ip);
-        this.logger.log("Site cadastrado: " + uri + " (" + ip + ")");
-    }
-
-    public String view() {
+    private String view() {
         HashTableViewer<URI, IPv4Address> viewer = new HashTableViewer<>(this.table);
         return viewer.view();
     }
 
-    public IPv4Address findSite(Scanner scanner) throws NoSuchElementException {
+    private IPv4Address findSite(Scanner scanner) throws NoSuchElementException, IllegalArgumentException {
         System.out.println("| Buscar IP do site...");
 
         System.out.print("URI: ");
@@ -71,7 +95,7 @@ public class DNSSimulator {
         return this.table.find(uri);
     }
 
-    public void removeSite(Scanner scanner) throws NoSuchElementException {
+    private void removeSite(Scanner scanner) throws NoSuchElementException {
         System.out.println("| Remover site...");
 
         System.out.print("URI: ");
@@ -81,45 +105,56 @@ public class DNSSimulator {
         this.logger.log("Site removido: " + uri);
     }
 
-    public void interact(Scanner scanner) {
+    private void interact(Scanner scanner) {
         System.out.println(present() + "\n");
+        String input;
 
-        String input = scanner.nextLine();
-        input = input.toUpperCase();
+        this.logger.report();
 
-        while (!input.equals("Q")) {
+        do {
+            System.out.print("\n>> COMANDO: ");
+            input = scanner.nextLine();
+            input = input.toUpperCase();
 
             switch (input) {
                 case "C":
-                    this.addSite(scanner);
+                    this.logger.waitFor(() -> this.addSite(scanner));
                     break;
 
                 case "V":
-                    System.out.println(this.view());
+                    this.logger.waitFor(() -> System.out.println(this.view()));
                     break;
 
                 case "P":
-                    System.out.print("IP: ");
-                    try {
-                        IPv4Address result = this.findSite(scanner);
-                        System.out.println(result);
-                    } catch (NoSuchElementException e) {
-                        System.out.println(e.getMessage());
-                    }
+                    this.logger.waitFor(() -> {
+                        System.out.print("IP: ");
+                        try {
+                            IPv4Address result = this.findSite(scanner);
+                            System.out.println(result);
+                        } catch (NoSuchElementException | IllegalArgumentException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    });
+
                     break;
 
                 case "R":
-                    try {
-                        this.removeSite(scanner);
-                    } catch (NoSuchElementException e) {
-                        System.out.println(e.getMessage());
-                    }
+                    this.logger.waitFor(() -> {
+                        try {
+                            this.removeSite(scanner);
+                        } catch (NoSuchElementException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    });
+                    break;
+
+                case "Q":
                     break;
 
                 default:
                     try {
                         int steps = Integer.parseInt(input);
-                        this.addSites(steps);
+                        this.logger.waitFor(() -> this.addSites(steps));
                     } catch (NumberFormatException e) {
                         System.out.println("Opção desconhecida. Tente novamente.");
                     }
@@ -127,9 +162,12 @@ public class DNSSimulator {
             }
 
             this.logger.report();
+        } while (!input.equals("Q"));
+    }
 
-            input = scanner.nextLine();
-            input = input.toUpperCase();
-        }
+    public void run(Scanner scanner) {
+        this.preset();
+        this.interact(scanner);
+        this.end();
     }
 }
